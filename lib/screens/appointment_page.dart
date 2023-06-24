f87da99b182c7680586de21877d5ebad6b5b579c
shinnkura <n.shinzo1010@icloud.com>
2023-06-24 14:16:09 +0900

    予約一覧ページの作成

--- lib/screens/appointment_page.dart
+++ lib/screens/appointment_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_and_laravel/utils/config.dart';
 
 class AppointmentPage extends StatefulWidget {
   const AppointmentPage({super.key});
@@ -7,11 +8,302 @@ class AppointmentPage extends StatefulWidget {
   State<AppointmentPage> createState() => _AppointmentPageState();
 }
 
+enum FilterStatus { upcomig, complete, cnacel }
+
 class _AppointmentPageState extends State<AppointmentPage> {
+  FilterStatus status = FilterStatus.upcomig;
+  Alignment _alignment = Alignment.centerLeft;
+  List<dynamic> schedules = [
+    {
+      "teacher_name": "さおり先生",
+      "teacher_profile": "assets/teacher_1.jpg",
+      "category": "Flutter",
+      "status": FilterStatus.upcomig,
+    },
+    {
+      "teacher_name": "いおり先生",
+      "teacher_profile": "assets/teacher_2.jpg",
+      "category": "Flutter",
+      "status": FilterStatus.complete,
+    },
+    {
+      "teacher_name": "みさき先生",
+      "teacher_profile": "assets/teacher_3.jpg",
+      "category": "Flutter",
+      "status": FilterStatus.complete,
+    },
+    {
+      "teacher_name": "あゆみ先生",
+      "teacher_profile": "assets/teacher_4.jpg",
+      "category": "Flutter",
+      "status": FilterStatus.cnacel,
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Appointment Page'),
+    List<dynamic> filteredSchedules = schedules.where((var schedule) {
+      // switch (schedule['status']) {
+      //   case 'upcoming':
+      //     return status == FilterStatus.upcomig;
+      //     break;
+      //   // schedules['status'] = FilterStatus.upcomig;
+      //   // break;
+      //   case 'complete':
+      //     return status == FilterStatus.complete;
+      //     break;
+      //   case 'cancel':
+      //     return status == FilterStatus.cnacel;
+      //     break;
+      //   default:
+      // }
+      return schedule['status'] == status;
+    }).toList();
+    return SafeArea(
+      child: Padding(
+        padding: EdgeInsets.only(
+          left: 20,
+          right: 20,
+          top: 20,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(
+              '予約一覧',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Config.spaceSmall,
+            Stack(
+              children: [
+                Container(
+                  width: double.infinity,
+                  height: 40,
+                  decoration: BoxDecoration(
+                    color: Colors.grey[100],
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      for (FilterStatus filterStatus in FilterStatus.values)
+                        Expanded(
+                          child: GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                if (filterStatus == FilterStatus.upcomig) {
+                                  status = FilterStatus.upcomig;
+                                  _alignment = Alignment.centerLeft;
+                                } else if (filterStatus ==
+                                    FilterStatus.complete) {
+                                  status = FilterStatus.complete;
+                                  _alignment = Alignment.center;
+                                } else {
+                                  status = FilterStatus.cnacel;
+                                  _alignment = Alignment.centerRight;
+                                }
+                              });
+                            },
+                            child: Center(
+                              child: Text(filterStatus.name),
+                            ),
+                          ),
+                        ),
+                    ],
+                  ),
+                ),
+                AnimatedAlign(
+                  alignment: _alignment,
+                  duration: const Duration(milliseconds: 300),
+                  child: Container(
+                    width: 100,
+                    height: 40,
+                    decoration: BoxDecoration(
+                      color: Config.primaryColor,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Center(
+                      child: Text(
+                        status.name,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Config.spaceSmall,
+            Expanded(
+              child: ListView.builder(
+                itemCount: filteredSchedules.length,
+                itemBuilder: ((context, index) {
+                  var _schedule = filteredSchedules[index];
+                  bool isLastElement = filteredSchedules.length + 1 == index;
+                  return Card(
+                    shape: RoundedRectangleBorder(
+                      side: BorderSide(
+                        color: Colors.grey,
+                      ),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    margin: !isLastElement
+                        ? EdgeInsets.only(bottom: 20)
+                        : EdgeInsets.zero,
+                    child: Padding(
+                      padding: EdgeInsets.all(20),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          Row(
+                            children: [
+                              CircleAvatar(
+                                radius: 30,
+                                backgroundImage:
+                                    AssetImage(_schedule['teacher_profile']),
+                              ),
+                              // Config.spaceSmall,
+                              const SizedBox(
+                                width: 10,
+                              ),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    _schedule['teacher_name'],
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.w700,
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    height: 5,
+                                  ),
+                                  Text(
+                                    _schedule['category'],
+                                    style: TextStyle(
+                                      color: Colors.grey,
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 15,
+                          ),
+                          ScheduleCard(
+                            date: '2021/10/10',
+                            day: '日',
+                            time: '10:00-11:00',
+                          ),
+                          const SizedBox(
+                            height: 15,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Expanded(
+                                child: OutlinedButton(
+                                  onPressed: () {},
+                                  child: Text('キャンセル'),
+                                ),
+                              ),
+                              SizedBox(
+                                width: 20,
+                              ),
+                              Expanded(
+                                child: OutlinedButton(
+                                  style: OutlinedButton.styleFrom(
+                                    backgroundColor: Config.primaryColor,
+                                  ),
+                                  onPressed: () {},
+                                  child: Text(
+                                    '予約変更',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                }),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ScheduleCard extends StatelessWidget {
+  const ScheduleCard(
+      {Key? key, required this.date, required this.day, required this.time})
+      : super(key: key);
+  final String date;
+  final String day;
+  final String time;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.grey.shade100,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      width: double.infinity,
+      padding: const EdgeInsets.all(20),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          const Icon(
+            Icons.calendar_today,
+            color: Config.primaryColor,
+            size: 15,
+          ),
+          const SizedBox(
+            width: 5,
+          ),
+          Text(
+            '$day, $date',
+            style: const TextStyle(
+              color: Config.primaryColor,
+            ),
+          ),
+          const SizedBox(
+            width: 20,
+          ),
+          const Icon(
+            Icons.access_alarm,
+            color: Config.primaryColor,
+            size: 17,
+          ),
+          const SizedBox(
+            width: 5,
+          ),
+          Flexible(
+              child: Text(
+            time,
+            style: const TextStyle(
+              color: Config.primaryColor,
+            ),
+          ))
+        ],
+      ),
     );
   }
 }
